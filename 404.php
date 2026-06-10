e64144b94ea06b524082b418429382483d6611af
Vladimir Torres Muñoz <75955208+vtorresmz@users.noreply.github.com>
2026-06-10 14:09:12 -0400

    añadir clase demo a archivo

--- 404.php
+++ 404.php
@@ -10,7 +10,7 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main clase-demo">
 
 		<section class="error-404 not-found">
 			<header class="page-header">
